d9db581c8fbeb2bc892c0e18e98a64252a58cafc
Sovanroth <sovanrothnath@Sovanroths-MacBook-Pro.local>
2023-03-09 18:29:56 +0700

    update

---
+++ src/Employee.java
@@ -0,0 +1,44 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double monthlySalary;
+
+    public Employee(String firstName, String lastName, double monthlySalary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setMonthlySalary(monthlySalary);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    public double getYearlySalary() {
+        return monthlySalary * 12;
+    }
+
+    public void raiseSalary(double percentage) {
+        double raiseAmount = monthlySalary * percentage / 100;
+        monthlySalary += raiseAmount;
+    }
+}
